30ff0046e1bf33b552bf99d62730096dc8da2c66
Victor Colmenares <colmenaresvictor2797@gmail.com>
2022-02-16 16:45:10 -0400

    fixing css

--- src/Componentes/Main/Main.tsx
+++ src/Componentes/Main/Main.tsx
@@ -2,18 +2,13 @@ import "../Main/Main.css";
 
 export const Main = () => {
   return (
-    <div className="main">
+    <div className="main" style={{ backgroundImage: "url(/background.jpg)" }}>
       <img
         className="img-main"
         src={"./PANADERIA CARABOBO LOGO (1).png"}
         alt="Panaderia"
       ></img>
       <br></br>
-      <img
-        className="img-background"
-        src={"./134281536133043.jpg"}
-        alt="Panaderia"
-      ></img>
     </div>
   );
 };
